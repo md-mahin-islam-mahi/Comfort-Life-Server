bfe57ea96bcce6582ff6d72ff6dbf306ad015934
mahin-wd <wdmahin.mhm@gmail.com>
2022-11-23 21:46:49 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +11,21 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Comfore Life server is running")
 });
+
+// mongoDB 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4lqljgn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
