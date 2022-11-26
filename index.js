bfe6ecaac30629c2f1179dbe592c6f161e7a59cc
mahin-wd <wdmahin.mhm@gmail.com>
2022-11-26 19:51:20 +0600

    added admin interection

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
         const userCollection = client.db("comfortLife").collection("users");
         const allFurniture = client.db("comfortLife").collection("furnitureItems");
         const furnitureCategories = client.db("comfortLife").collection("categories");
+        const orderCollection = client.db("comfortLife").collection("orders");
 
         // users
         app.post("/users", async (req, res) => {
@@ -34,6 +35,23 @@ async function run() {
             res.send(users);
         });
 
+        //delete user
+        app.delete("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const users = await userCollection.deleteOne(query);
+            res.send(users);
+        });
+
+        // get users by type
+        app.get("/users-type/:type", async (req, res) => {
+            const type = req.params.type;
+            const query = { type: type };
+            const users = await userCollection.find(query).toArray();
+            res.send(users);
+        });
+
+        // get users by email
         app.get("/users/:email", async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
@@ -72,10 +90,46 @@ async function run() {
             res.send(result);
         })
 
+        // get all furniture
+        app.get("/furniture", async (req, res) => {
+            const query = {};
+            const furnitures = await allFurniture.find(query).toArray();
+            res.send(furnitures);
+        });
+
+        // furniture get thorough email
+        app.get("/get-furniture/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { previousId: id };
+            const furnitures = await orderCollection.findOne(query);
+            res.send(furnitures);
+        });
+
+        // get furniture by availability
+        app.get("/furniture-available", async (req, res) => {
+            const query = { isAvailable: "true" };
+            const furnitures = await allFurniture.find(query).toArray();
+            res.send(furnitures);
+        });
+
+        // update product
+        app.put("/furniture/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDock = {
+                $set: {
+                    isAvailable: 'true',
+                }
+            }
+            const furnitures = await allFurniture.updateOne(query, updateDock, options);
+            res.send(furnitures);
+        });
+
         // furnitureItems
         app.get("/furniture/:email", async (req, res) => {
             const email = req.params.email;
-            const query = {email: email};
+            const query = { email: email };
             const furnitures = await allFurniture.find(query).toArray();
             res.send(furnitures);
         });
@@ -84,7 +138,45 @@ async function run() {
         app.post("/furniture", async (req, res) => {
             const furniture = req.body;
             const result = await allFurniture.insertOne(furniture);
-            return res.send(result);
+            res.send(result);
+        });
+
+        // add orders
+        app.post("/orders", async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        // get orders collection by email
+        app.get("/orders", async (req, res) => {
+            const email = req.query.email;
+            const query = { buyerEmail: email };
+            const orders = await orderCollection.find(query).toArray();
+            res.send(orders);
+        });
+
+        // delete order
+        app.delete("/orders/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const orders = await orderCollection.deleteOne(query);
+            res.send(orders);
+        });
+
+        // payment methods
+        app.put("/payment/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDock = {
+                $set: {
+                    payment: 'true',
+                    isAvailable: 'false'
+                }
+            }
+            const paid = await allFurniture.updateOne(query, updateDock, options);
+            res.send(paid);
         })
     }
     finally {
