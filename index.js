017cfd662fb2478b0678c876db7c00233957d72c
mahin-wd <wdmahin.mhm@gmail.com>
2022-11-26 20:53:29 +0600

    added access token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
