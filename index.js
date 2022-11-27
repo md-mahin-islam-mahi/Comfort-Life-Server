d1e9dccb9e837a9b90b0903434a07cb5aa6cbd3a
mahin-wd <wdmahin.mhm@gmail.com>
2022-11-27 08:54:17 +0600

    passed jwt to client

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.get('/', (req, res) => {
     res.send("Comfore Life server is running")
 });
 
+app.post('/jwt', (req, res) => {
+    const user = req.body;
+    const token =  jwt.sign(user, process.env.JSON_WEB_TOKEN, {expiresIn: "10h"});
+    console.log(token);
+    res.send({token});
+});
+
 // mongoDB 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4lqljgn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
